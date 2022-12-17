3e457c73f498d3ffe97e26a86b076252dc18713d
Prerit Dayal <prerit714@gmail.com>
2022-12-17 17:36:00 +0530

    Make colorscheme transparent

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,13 @@
-local colorscheme = "tokyonight-night"
+local colorscheme = "tokyonight"
+local config_status_ok, nvim_tree_config = pcall(require, "tokyonight")
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
+nvim_tree_config.setup({
+  style = "night",
+  transparent = true
+})
+
+local status_ok_for_colorscheme, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if not status_ok_for_colorscheme or config_status_ok then
   return
 end
